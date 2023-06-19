103a27fc610a164ab22c6f753ffd79a9b0d43998
Hasbi Ali Firdaus <hasbialifirdaus001@gmail.com>
2023-06-19 14:06:43 +0700

    initial commit

---
+++ src/components/IndexHome.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-container class="no-padding">
+      <v-row class="text-center">
+        <v-col cols="4">
+          <v-carousel :show-arrows="false">
+            <v-carousel-item
+              v-for="(item,i) in items"
+              :key="i"
+              :src="item.src"
+              cover
+            ></v-carousel-item>
+          </v-carousel>
+        </v-col>
+        <v-col cols="8">
+            <h3>form</h3>
+        </v-col>
+      </v-row>
+    </v-container>
+</template>
+<script>
+  export default {
+    name: 'IndexHome',
+
+    data: () => ({
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader',
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify',
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        },
+      ],
+      items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+        ],
+    }),
+  }
+</script>
